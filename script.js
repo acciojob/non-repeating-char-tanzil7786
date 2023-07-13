a78e90baf8d36f5c597ae347f11c98bfb3676104
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-07-13 10:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your JS code here. If required.
+function firstNonRepeatingCharacter(inputString) {
+  const charFrequencies = {};
+
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString[i];
+    charFrequencies[char] = (charFrequencies[char] || 0) + 1;
+  }
+
+  for (let i = 0; i < inputString.length; i++) {
+    const char = inputString[i];
+    if (charFrequencies[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
+
+// Prompt the user for input and call the function
+const inputString = prompt("Enter a string:");
+const result = firstNonRepeatingCharacter(inputString);
+
+// Display the result using alert
+alert("First non-repeating character: " + result);
